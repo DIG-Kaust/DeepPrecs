980f81388363af1db78650c2f7189d4d7a6a9d44
mrava87 <matteoravasi@gmail.com>
2022-08-25 16:17:50 +0300

    Cleaned environment files

--- install_env.sh
+++ install_env.sh
@@ -15,10 +15,13 @@ conda activate deepprecs
 conda env list
 echo 'Created and activated environment:' $(which python)
 
+# remove numba (temporary, due to collision with numpy version)
+rm -rf /home/ravasim/miniconda3/envs/deepprecs/lib/python3.10/site-packages/numba*
+
 # install deepprecs package
 pip install -e .
 
-# check cupy works as expected
+# check cupy-torch work as expected
 echo 'Checking cupy version and running a command...'
 python -c 'import cupy as cp; print(cp.__version__); import torch; print(torch.__version__);  print(torch.cuda.get_device_name(torch.cuda.current_device())); print(torch.ones(10).to("cuda:0"))'
 
